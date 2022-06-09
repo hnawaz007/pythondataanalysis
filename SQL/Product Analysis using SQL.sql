713aebed45f98c3c3ab59385aaac42f5c8b9a2ac
Haq Nawaz <66388243+hnawaz007@users.noreply.github.com>
2022-06-09 13:56:56 -0400

    Update Product Analysis using SQL.sql

--- SQL/Product Analysis using SQL.sql
+++ SQL/Product Analysis using SQL.sql
@@ -1,7 +1,7 @@
 --Sales by Products
 Select
-		p.EnglishProductName AS product_name,
-	   SUM(f.SalesAmount) AS sales_amount
+	p.EnglishProductName AS product_name,
+	SUM(f.SalesAmount) AS sales_amount
 from DimProduct p
 inner join FactInternetSales f on p.ProductKey = f.ProductKey
 Group By p.EnglishProductName
@@ -9,8 +9,8 @@ Order by SUM(f.SalesAmount) DESC
 
 --Top 10 Products by Sales
 Select top 10 
-		p.EnglishProductName AS product_name,
-	   SUM(f.SalesAmount) AS sales_amount
+	p.EnglishProductName AS product_name,
+	SUM(f.SalesAmount) AS sales_amount
 from DimProduct p
 inner join FactInternetSales f on p.ProductKey = f.ProductKey
 Group By p.EnglishProductName
@@ -18,8 +18,8 @@ Order by SUM(f.SalesAmount) DESC
 
 --Products by Lowest Production Cost
 Select top 10 
-		p.EnglishProductName AS product_name,
-	   SUM(f.TotalProductCost) AS sales_amount
+	p.EnglishProductName AS product_name,
+	SUM(f.TotalProductCost) AS sales_amount
 from DimProduct p
 inner join FactInternetSales f on p.ProductKey = f.ProductKey
 Group By p.EnglishProductName
@@ -27,8 +27,8 @@ Order by SUM(f.TotalProductCost) ASC
 
 --Sales by Product Category
 Select 
-	   pc.EnglishProductCategoryName AS product_category,
-       SUM(f.SalesAmount) AS total_sales
+	pc.EnglishProductCategoryName AS product_category,
+        SUM(f.SalesAmount) AS total_sales
 From DimProduct p
 inner join DimProductSubcategory ps on p.ProductSubcategoryKey = ps.ProductSubcategoryKey
 inner join DimProductCategory pc on ps.ProductCategoryKey = pc.ProductCategoryKey
